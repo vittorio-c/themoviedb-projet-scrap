6b825e5463a9e44bbdd54a159ce05f01600a56f1
Vittorio C <vittorio@chillemi.fr>
2021-04-01 15:29:39 +0200

    Basic CRUD operations on Movie collection

--- queries/movies.py
+++ queries/movies.py
@@ -1,32 +1,28 @@
 from connection.client import client
 
-# Here goes all the queries for fetching movies from DB
+movie_collection = client.themoviedb.movies
+
+def get_movie(id):
+    movie = movie_collection.find({'_id': id})
+    return movie[0]
+
+def get_movies_paginated(page_size, page_num):
+    """
+    returns a set of documents belonging to page number `page_num`
+    where size of each page is `page_size`.
+    """
+    # Calculate number of documents to skip
+    skips = page_size * (page_num - 1)
+    # Skip and limit
+    cursor = movie_collection.find().skip(skips).limit(page_size)
+    # Return documents
+    return [movie for movie in cursor]
 
 def get_movies():
-    movies = client.metflix.movies.find()
-    return movies
+    cursor = movie_collection.find()
+    return [movie for movie in cursor]
 
-def get_movies_with_artists():
-    movies = client.metflix.movies
-    aggregate = [
-        {
-            "$lookup": {
-                "from": "artists",
-                'localField' : 'director._id',
-                'foreignField' : '_id',
-                'as' : 'director_qry'
-                },
-            },
-        {
-            "$lookup" :{
-                "from": "artists",
-                'localField' : 'actors._id',
-                'foreignField' : '_id',
-                'as' : 'actors_qry'
-                }
-            }
-        ]
-
-    movies = movies.aggregate(aggregate)
-    return movies
-
+# TODO : checker que movie_object est bien du json
+# sinon le traiter comme un dico python ou le transformer en Json
+def insert_or_update_movie(movie_object):
+    return movie_collection.replace_one({'_id': movie_object['_id']}, movie_object, upsert=True)
