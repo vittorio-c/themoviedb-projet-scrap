8c399c5d24b53c014b65917fa0bf68c2ca018105
vittorio-c <dev@eltov.xyz>
2021-12-15 11:19:21 +0100

    Add 10 tests for pagination

--- tests/units/pagination.py
+++ tests/units/pagination.py
@@ -26,10 +26,45 @@ def movie_col(load_movie_json):
     yield collection
 
 
-def test_it_can_paginate_over_results(monkeypatch, movie_col):
+def test_it_can_paginate_with_correct_page_size(monkeypatch, movie_col):
     query_movie.movie_collection = movie_col
+    movie_results = query_movie.get_movies_paginated(15, 1)
 
-    movie_results = query_movie.get_movies_paginated(5, 1)
+    assert len(movie_results) == 15
 
-    assert len(movie_results) == 5
-    # assert movie_results[0]['title'] == movie_col.find()[0]['title']
+
+def test_it_can_paginate_with_correct_page_size_and_correct_page_number(
+    monkeypatch, movie_col
+):
+    query_movie.movie_collection = movie_col
+    first_page = query_movie.get_movies_paginated(15, 1)
+    second_page = query_movie.get_movies_paginated(15, 2)
+
+    # Use set operations to assert that no movies
+    # of first page are in second page. If true,
+    # this means pagination is successfull.
+    first = {movie["_id"] for movie in first_page}
+    second = {movie["_id"] for movie in second_page}
+    assert len(first & second) == 0
+    assert len(second_page) == 15
+
+
+@pytest.mark.parametrize(
+    "sort, order, reverse",
+    [
+        ("release_year", "1", False),
+        ("release_year", "-1", True),
+        ("title", "1", False),
+        ("title", "-1", True),
+        ("user_rating", "1", False),
+        ("user_rating", "-1", True),
+        ("budget", "1", False),
+        ("budget", "-1", True),
+    ],
+)
+def test_it_can_paginate_and_sort(monkeypatch, movie_col, sort, order, reverse):
+    query_movie.movie_collection = movie_col
+    movie_results = query_movie.get_movies_paginated(15, 1, sort, order)
+    ordered_years = [movie[sort] for movie in movie_results]
+
+    assert sorted(ordered_years, reverse=reverse) == ordered_years
