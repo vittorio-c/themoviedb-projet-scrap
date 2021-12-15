5d4e4723fbdca3ef5bd56b88002f6b90bcbc0668
Guillaume <guillaume.waroux@gmail.com>
2021-12-15 11:37:49 +0100

    update the test script, with best practices and github actions can execute the tests

--- tests/scrapper/test_scrapper.py
+++ tests/scrapper/test_scrapper.py
@@ -1,14 +1,29 @@
+import time
+
 import pytest
 from dotenv import dotenv_values
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def instantiate_driver(request):
-    config = dict(dotenv_values("../../.env.test"))
-    print(config)
-    web_driver = webdriver.Chrome("../../drivers/chromedriver-96.exe")
+    # Define an agent to scrap the website else we are blocked
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
+    # Declare many options to execute the tests in github Actions
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument(f"user-agent={user_agent}")
+    # The old method is deprecated, this method load the webdriver in browser cache
+    s = Service(ChromeDriverManager().install())
+    # Create the webdriver
+    web_driver = webdriver.Chrome(service=s, options=options)
     request.cls.driver = web_driver
     yield
     web_driver.close()
@@ -24,12 +39,9 @@ class TestScrapper(BasicTest):
         the_moviedb_base_url = "https://www.themoviedb.org/"
         self.driver.get(the_moviedb_base_url)
         try:
-            cookie_button = self.driver.find_element_by_css_selector(
-                "#cookie_notice p:nth-child(2) a"
+            cookie_button = self.driver.find_element(
+                By.CSS_SELECTOR, "#cookie_notice p:nth-child(2) a"
             )
             assert 1
         except:
             assert False
-
-        # cookie_button = self.driver.find_element(By.CSS_SELECTOR('#cookie_notice p:nth-child(2) a'))
-        # print(cookie_button)
