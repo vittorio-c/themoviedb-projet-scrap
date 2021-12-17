28793d8ade7d917f9e7cc6feef137d388c379075
bboucdevos <bboucdevos@webnet.fr>
2021-12-17 17:14:14 +0100

    correction

--- tests/units/test_routes.py
+++ tests/units/test_routes.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 def test_api_movies(http_client):
     response = http_client.get("/api/movies")
     assert response.status_code == 200
+
 
 def test_routing(http_client):
     for rule in app.url_map.iter_rules():
